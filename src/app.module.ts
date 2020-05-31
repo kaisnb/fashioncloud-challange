280f661c6154d634ecc2214011c03c4832496e2a
kaisnb <kai.schoennerger95@googlemail.com>
2020-05-31 15:11:07 +0200

    feat: added config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CacheModule } from './cache/cache.module';
 
+/**
+ * Set APP_ENV environment variable to load
+ * a different .env file.
+ */
+const env = process.env.APP_ENV || 'dev';
+
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `env/.${env}.env`,
+    }),
     MongooseModule.forRoot(
       'mongodb+srv://developer:VBg5vSmFitW4QC8O@cluster0-puo7u.mongodb.net/fashion-cloud-challange?retryWrites=true&w=majority',
     ),
--- src/cache/cache.service.ts
+++ src/cache/cache.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CacheEntry, CacheEntryDoc } from './interfaces/cache-entry.interface';
 
 @Injectable()
 export class CacheService {
+  private readonly LIMIT =
+    parseInt(this.configService.get('CACHE_SIZE'), 10) || 100000;
+
   constructor(
+    private configService: ConfigService,
     @InjectModel('CacheEntry') private cacheEntryModel: Model<CacheEntryDoc>,
   ) {}
 
